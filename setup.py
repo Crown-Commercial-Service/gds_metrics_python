ac9dd49342417251a47cb20eade4db2c35a0ea3c
Alexey Bezhan <al@fueledbylemons.com>
2018-04-12 14:07:13 +0100

    Add setup.py and move dependencies from requirements.txt
    Adds a version.py file used to specify current package version.
    Adds setup.py and moves the list of dependencies from requirements.txt
    to `install_requires`.
    
    setup.py specifies all library dependencies and requirements.txt
    installs gds_metrics as an editable package and will contain development
    dependencies in the future.

---
+++ setup.py
@@ -0,0 +1,30 @@
+"""
+Library for Prometheus instrumentation in Python/Flask based apps
+"""
+
+import re
+import ast
+from setuptools import setup, find_packages
+
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('gds_metrics/version.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+setup(
+    name='gds-metrics',
+    version=version,
+    url='https://github.com/alphagov/gds_metrics_python',
+    license='MIT',
+    author='Government Digital Service',
+    description='Library for Prometheus instrumentation in Python/Flask based apps',
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "prometheus_client==0.2.0",
+        "Flask>=0.10"
+    ]
+)
