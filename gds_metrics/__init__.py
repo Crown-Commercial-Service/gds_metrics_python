db936a6d01d690dcdb17a6933d9511ccf4e13102
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2018-04-12 13:52:29 +0100

    Added metrics and handlers

--- gds_metrics/__init__.py
+++ gds_metrics/__init__.py
@@ -1,5 +1,7 @@
+from time import monotonic
 import os
-from flask import request, Response
+from flask import g, request, Response
+from flask.signal import got_request_exception, request_finished
 
 import prometheus_client
 from prometheus_client import Counter, Histogram
@@ -17,11 +19,59 @@ class GDSMetrics(object):
         self.registry = CollectorRegistry()
         multiprocess.MultiProcessCollector(self.registry)
 
-    def init_app(self, app):
-        app.add_url_rule(self.metrics_path, 'metrics', self.metrics)
+        self.HTTP_SERVER_REQUESTS_TOTAL = Counter(
+            'http_server_requests_total',
+            'Total requests',
+            ['method', 'host', 'path', 'code'],
+            registry=self.registry,
+        )
 
-    def metrics(self):
+        self.HTTP_SERVER_EXCEPTIONS_TOTAL = Counter(
+            'http_server_exceptions_total',
+            'Total number of exceptions',
+            ['error'],
+            registry=self.registry,
+        )
+
+        self.HTTP_SERVER_REQUEST_DURATION_SECONDS = Histogram(
+            'http_server_request_duration_seconds', 'Server request duration in seconds',
+            ['method', 'host', 'path', 'code'],
+            registry=self.registry,
+        )
+
+    def init_app(self, app):
+        app.add_url_rule(self.metrics_path, 'metrics', self.metrics_endpoint)
+
+        app.before_request(self.before_request)
+        request_finished.connect(self.teardown_request, sender=app)
+        got_request_exception.connect(self.handle_exception, sender=app)
+
+    def metrics_endpoint(self):
         return Response(
             prometheus_client.generate_latest(self.registry),
             mimetype='text/plain; version=0.0.4; charset=utf-8'
         )
+
+    def before_request(self, *args, **kwargs):
+        g._gds_metrics_start_time = monotonic()
+
+    def teardown_request(self, sender, response, *args, **kwargs):
+        resp_time = monotonic() - g._gds_metrics_start_time
+        self.HTTP_SERVER_REQUEST_DURATION_SECONDS.labels(
+            request.method,
+            request.host,
+            request.path,
+            response.status_code
+        ).observe(resp_time)
+
+        self.HTTP_SERVER_REQUESTS_TOTAL.labels(
+            request.method,
+            request.host,
+            request.path,
+            response.status_code
+        ).inc()
+
+        return response
+
+    def handle_exception(self, sender, response, *args, **kwargs):
+        self.HTTP_SERVER_EXCEPTIONS_TOTAL.labels(kwargs.get('exc_info', [None])[0]).inc()
